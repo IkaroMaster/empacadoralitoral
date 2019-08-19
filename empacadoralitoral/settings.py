f3ba51ec50c5c0517e681ba8696866c71bf9cfe3
obek <jorgeescoto18@gmail.com>
2019-08-19 17:21:43 -0600

    cc

--- empacadoralitoral/settings.py
+++ empacadoralitoral/settings.py
@@ -195,7 +195,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = (
     # os.path.join(BASE_DIR, 'global_static'),
 # )
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static/'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticos/')
 TEMPLATE_DIRS=(BASE_DIR, 'templates')
